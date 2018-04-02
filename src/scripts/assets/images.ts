702db5876121278378f22dbf2022df501caae624
atomita <sleeping.cait.sith+gh@gmail.com>
2018-04-02 15:18:42 +0900

    fix: Compliant with parcel v1.7

--- src/scripts/assets/images.ts
+++ src/scripts/assets/images.ts
@@ -1,32 +1,32 @@
-import * as avatar from 'admin-lte/dist/img/avatar.png'
-import * as avatar04 from 'admin-lte/dist/img/avatar04.png'
-import * as avatar2 from 'admin-lte/dist/img/avatar2.png'
-import * as avatar3 from 'admin-lte/dist/img/avatar3.png'
-import * as avatar5 from 'admin-lte/dist/img/avatar5.png'
-import * as boxed_bg_jpg from 'admin-lte/dist/img/boxed-bg.jpg'
-import * as boxed_bg_png from 'admin-lte/dist/img/boxed-bg.png'
-import * as default_50x50 from 'admin-lte/dist/img/default-50x50.gif'
-import * as icons from 'admin-lte/dist/img/icons.png'
-import * as photo1 from 'admin-lte/dist/img/photo1.png'
-import * as photo2 from 'admin-lte/dist/img/photo2.png'
-import * as photo3 from 'admin-lte/dist/img/photo3.jpg'
-import * as photo4 from 'admin-lte/dist/img/photo4.jpg'
-import * as user1_128x128 from 'admin-lte/dist/img/user1-128x128.jpg'
-import * as user2_160x160 from 'admin-lte/dist/img/user2-160x160.jpg'
-import * as user3_128x128 from 'admin-lte/dist/img/user3-128x128.jpg'
-import * as user4_128x128 from 'admin-lte/dist/img/user4-128x128.jpg'
-import * as user5_128x128 from 'admin-lte/dist/img/user5-128x128.jpg'
-import * as user6_128x128 from 'admin-lte/dist/img/user6-128x128.jpg'
-import * as user7_128x128 from 'admin-lte/dist/img/user7-128x128.jpg'
-import * as user8_128x128 from 'admin-lte/dist/img/user8-128x128.jpg'
+import avatar from 'admin-lte/dist/img/avatar.png'
+import avatar04 from 'admin-lte/dist/img/avatar04.png'
+import avatar2 from 'admin-lte/dist/img/avatar2.png'
+import avatar3 from 'admin-lte/dist/img/avatar3.png'
+import avatar5 from 'admin-lte/dist/img/avatar5.png'
+import boxed_bg_jpg from 'admin-lte/dist/img/boxed-bg.jpg'
+import boxed_bg_png from 'admin-lte/dist/img/boxed-bg.png'
+import default_50x50 from 'admin-lte/dist/img/default-50x50.gif'
+import icons from 'admin-lte/dist/img/icons.png'
+import photo1 from 'admin-lte/dist/img/photo1.png'
+import photo2 from 'admin-lte/dist/img/photo2.png'
+import photo3 from 'admin-lte/dist/img/photo3.jpg'
+import photo4 from 'admin-lte/dist/img/photo4.jpg'
+import user1_128x128 from 'admin-lte/dist/img/user1-128x128.jpg'
+import user2_160x160 from 'admin-lte/dist/img/user2-160x160.jpg'
+import user3_128x128 from 'admin-lte/dist/img/user3-128x128.jpg'
+import user4_128x128 from 'admin-lte/dist/img/user4-128x128.jpg'
+import user5_128x128 from 'admin-lte/dist/img/user5-128x128.jpg'
+import user6_128x128 from 'admin-lte/dist/img/user6-128x128.jpg'
+import user7_128x128 from 'admin-lte/dist/img/user7-128x128.jpg'
+import user8_128x128 from 'admin-lte/dist/img/user8-128x128.jpg'
 
-import * as credit_american_express from 'admin-lte/dist/img/credit/american-express.png'
-import * as credit_cirrus from 'admin-lte/dist/img/credit/cirrus.png'
-import * as credit_mastercard from 'admin-lte/dist/img/credit/mastercard.png'
-import * as credit_mestro from 'admin-lte/dist/img/credit/mestro.png'
-import * as credit_paypal from 'admin-lte/dist/img/credit/paypal.png'
-import * as credit_paypal2 from 'admin-lte/dist/img/credit/paypal2.png'
-import * as credit_visa from 'admin-lte/dist/img/credit/visa.png'
+import credit_american_express from 'admin-lte/dist/img/credit/american-express.png'
+import credit_cirrus from 'admin-lte/dist/img/credit/cirrus.png'
+import credit_mastercard from 'admin-lte/dist/img/credit/mastercard.png'
+import credit_mestro from 'admin-lte/dist/img/credit/mestro.png'
+import credit_paypal from 'admin-lte/dist/img/credit/paypal.png'
+import credit_paypal2 from 'admin-lte/dist/img/credit/paypal2.png'
+import credit_visa from 'admin-lte/dist/img/credit/visa.png'
 
 export {
   avatar,
@@ -50,12 +50,11 @@ export {
   user6_128x128,
   user7_128x128,
   user8_128x128,
-
   credit_american_express,
   credit_cirrus,
   credit_mastercard,
   credit_mestro,
   credit_paypal,
   credit_paypal2,
-  credit_visa,
+  credit_visa
 }
