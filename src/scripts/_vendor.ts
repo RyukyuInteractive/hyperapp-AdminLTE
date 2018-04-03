52a19212120be35a5e5140771b9dd5d250eee576
atomita <sleeping.cait.sith+gh@gmail.com>
2018-04-03 16:03:13 +0900

    fix: Import jquery, bootstrap and admin-lte

--- src/scripts/_vendor.ts
+++ src/scripts/_vendor.ts
@@ -1,7 +1,3 @@
-import { app, h } from 'hyperapp'
-import { Container, decorate, inject, injectable, named, tagged } from 'inversify'
-import 'reflect-metadata'
-
 import 'admin-lte/dist/css/AdminLTE.css'
 import 'admin-lte/dist/css/skins/_all-skins.css'
 import 'admin-lte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.css'
@@ -12,3 +8,14 @@ import 'font-awesome/css/font-awesome.css'
 import 'ionicons/dist/css/ionicons.css'
 import 'jvectormap/jquery-jvectormap.css'
 import 'morris.js/morris.css'
+
+import { app, h } from 'hyperapp'
+import { Container, decorate, inject, injectable, named, tagged } from 'inversify'
+import 'reflect-metadata'
+
+import { default as jquery } from 'jquery'
+(window as any).jQuery = (window as any).$ = jquery
+
+import 'bootstrap'
+
+import 'admin-lte'
