e78e4018d314d724b5db70cc8de152fb4515058d
atomita <sleeping.cait.sith+gh@gmail.com>
2018-04-10 19:38:40 +0900

    fix: load order of css

--- src/scripts/_vendor.ts
+++ src/scripts/_vendor.ts
@@ -1,6 +1,3 @@
-import 'admin-lte/dist/css/AdminLTE.css'
-import 'admin-lte/dist/css/skins/_all-skins.css'
-import 'admin-lte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.css'
 import 'bootstrap-datepicker/dist/css/bootstrap-datepicker.css'
 import 'bootstrap-daterangepicker/daterangepicker.css'
 import 'bootstrap/dist/css/bootstrap.css'
@@ -8,6 +5,10 @@ import 'font-awesome/css/font-awesome.css'
 import 'ionicons/dist/css/ionicons.css'
 import 'jvectormap/jquery-jvectormap.css'
 import 'morris.js/morris.css'
+
+import 'admin-lte/dist/css/AdminLTE.css'
+import 'admin-lte/dist/css/skins/_all-skins.css'
+import 'admin-lte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.css'
 
 import 'hyperapp'
 import 'inversify'
